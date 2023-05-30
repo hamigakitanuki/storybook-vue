13d45fa128865c149c6ebae784053e70963fa7b4
hamigakitanuki <kakizaki0213@gmail.com>
2023-05-30 15:21:41 +0900

    単純なコンポーネント

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,10 @@
+import '../src/index.css';
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
